6a4042be0c1651e0ecd50a9742ebe1c612fa8536
Omar Freddy Nova Bustamante <omar.nova.2020@upb.edu.co>
2020-03-26 15:06:28 -0500

    Mi primer commit

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ Created on Thu Mar 26 14:59:42 2020
 @author: Omar
 """
 
+a=int(input("Ingrese el primer numero: "))
+c=int(input("Ingrese el segundo numero: "))
+
+print("El producto de los numeros es: " + str(a+c))
+print("El doble del primer numero es: " + str(a*2))
 
