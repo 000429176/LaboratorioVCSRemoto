4e209e60dd4d3f3aa92ef815000a7e21f24e65bd
Omar Freddy Nova Bustamante <omar.nova.2020@upb.edu.co>
2020-03-26 18:20:59 -0500

    Se añadió dos nuevas entradas y se corrigieron las 2 anteriores

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ Created on Thu Mar 26 14:59:42 2020
 @author: Omar
 """
 
-a=int(input("Ingrese el primer numero: "))
-c=int(input("Ingrese el segundo numero: "))
+a=int(input("Ingrese el a: "))
+c=int(input("Ingrese el b: "))
+b=int(input("Ingrese b: "))
+d=int(input("Ingrese d: "))
 
-print("El producto de los numeros es: " + str(a+c))
+print("El producto de los dos numeros es: " + str(a+c))
 print("El doble del primer numero es: " + str(a*2))
+
 
