631a5273792e9113d2f9e6b1b0478d0e4eddd542
Omar Freddy Nova Bustamante <omar.nova.2020@upb.edu.co>
2020-03-26 22:24:30 -0500

    Se agregan valores de entrada, el cual pide ingresar un numero de cuatro digitos. Adicionalmente, se realiza un condicional para confirmar que el numero es de 4 cifras

---
+++ preinforme9_OmarNova.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 22:09:54 2020
+
+@author: Omar
+"""
+
+#%% Punto 2
+
+n=(int(input("Ingrese un numero de 4 cifras: ")))
+
+if n>9999 :
+    n=(int(input("Ingrese un numero de 4 cifras: ")))
--- untitled4.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 26 18:29:15 2020
-
-@author: Omar
-
-"""
-
-print("Hola mundo")
