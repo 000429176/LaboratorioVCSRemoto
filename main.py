eada69afc5830881752946ab95ec9f6724ec9bd3
Omar Freddy Nova Bustamante <omar.nova.2020@upb.edu.co>
2020-03-27 08:47:13 -0500

    Solucion simple terminada

--- main.py
+++ main.py
@@ -16,3 +16,20 @@ print("El doble del primer numero es: " + str(a*2))
 print("El cuadrado de b es: " + str(b**2))
 print("La raiz cuadrada de d es: " + str(d**(1/2)))
 
+#%% Solucion simple
+
+a = int(input ("Ingrese el valor de a: "))
+b = int(input ("Ingrese el valor de b: "))
+c = int(input ("Ingrese el valor de c: "))
+
+d = (b*2)-(4*a*c)
+
+if d == 0 :
+    print("X1 y X2 equivalen a "+ str(-b/(2*a)))
+elif d>0 :
+    print("X1 es igual a: " + str(((-b+(d**(1/2)))/(2*a))))
+    print("X2 es igual a: " + str(((-b-(d**(1/2)))/(2*a))))
+
+else: 
+    print("No presenta solucion dentro del dominio de los reales")
+
