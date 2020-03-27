8826911f8ef6e523bc249e8a874a5d1418aea64a
Omar Freddy Nova Bustamante <omar.nova.2020@upb.edu.co>
2020-03-26 22:54:36 -0500

    Se corrige el condicional, ahora convertido en ciclo while, para confirmir si el numero es de 4 digitos

--- preinforme9_OmarNova.py
+++ preinforme9_OmarNova.py
@@ -9,7 +9,7 @@ Created on Thu Mar 26 22:09:54 2020
 
 n=(int(input("Ingrese un numero de 4 cifras: ")))
 
-if n>9999 and n<=999:
+while n>9999 or n<=999:
     n=(int(input("Ingrese un numero de 4 cifras: ")))
 
 d=n/1000
