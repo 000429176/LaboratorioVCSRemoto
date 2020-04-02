cb7ca9737e70a5a2b331f806d770867b73d5bd11
Omar Freddy Nova Bustamante <omar.nova.2020@upb.edu.co>
2020-04-01 21:06:56 -0500

    Se importa numpy y se define la funcion con el array con la utilidad operacional en millones de COP de Kellog's

--- Preinforme10_OmarNova.py
+++ Preinforme10_OmarNova.py
@@ -5,4 +5,10 @@ Created on Wed Apr  1 20:46:51 2020
 @author: Omar
 """
 
+import numpy as np
+
+def utilidad():
+    utilidad = np.array([27834, 23789, 30189, 30967, 32501, 32701, 31665, 17155, 4614, 834])
+    return utilidad
+
 
