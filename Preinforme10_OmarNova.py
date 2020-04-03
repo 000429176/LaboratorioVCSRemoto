b4ff711101baf2bbf655590de7fc34c79a78f99a
Omar Freddy Nova Bustamante <omar.nova.2020@upb.edu.co>
2020-04-02 20:01:03 -0500

    Se agrega funcion para dar solucion al ejercicio 1, calculando el promedio de los dos ultimos, los dos primeros y finalmente restandolos

--- Preinforme10_OmarNova.py
+++ Preinforme10_OmarNova.py
@@ -11,4 +11,19 @@ def utilidad():
     utilidad = np.array([27834, 23789, 30189, 30967, 32501, 32701, 31665, 17155, 4614, 834])
     return utilidad
 
+def difMediaUltPri(utilidad):
+    cant=len(utilidad)
+    prom1 = (utilidad[cant-1] + utilidad[cant-2])/2
+    prom2 = (utilidad[0] + utilidad[1])/2
+    dif = prom1 - prom2
+    return dif
 
+
+    
+
+
+
+
+
+utilidad=utilidad()
+print("La diferencia del promedio de los ultimos años y los primeros años es: " + str(difMediaUltPri(utilidad)))
