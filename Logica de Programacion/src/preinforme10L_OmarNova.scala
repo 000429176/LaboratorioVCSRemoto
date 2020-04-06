eaccfa814e07a06bc0d771052f931fa8ca58360e
Omar Freddy Nova Bustamante <omar.nova.2020@upb.edu.co>
2020-04-05 22:47:51 -0500

    Inicio del código

---
+++ Logica de Programacion/src/preinforme10L_OmarNova.scala
@@ -0,0 +1,13 @@
+object preinforme10L_OmarNova {
+  
+  //Array con las utilidades operacionales de Kellogg's entre los años 2008 a 2017
+  
+  def Utilidad(): Array[Int] = {
+		  var Utilidad = Array(27834, 23789, 30189, 30967, 32501, 32701, 31665, 17155, 4614, 834)
+  	  return Utilidad
+	}                                       
+	
+  
+
+  
+}
