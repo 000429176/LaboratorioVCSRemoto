9240610ae4cc3d1c0ca26ac406cc06b316cd5882
Omar Freddy Nova Bustamante <omar.nova.2020@upb.edu.co>
2020-04-05 22:50:31 -0500

    Se realiza desarrollo del ejercicio 1

--- Logica de Programacion/src/preinforme10L_OmarNova.scala
+++ Logica de Programacion/src/preinforme10L_OmarNova.scala
@@ -7,6 +7,15 @@ object preinforme10L_OmarNova {
   	  return Utilidad
 	}                                       
 	
+  //Función que cálcula el promedio de los últimos dos años, en la variable prom1; y los dos primeros años, en la variable prom2. Después realiza la resta del prom1 menos prom2, siendo la variable dif.
+  
+  def difMediaUltPri(Utility : Array[Int]): Int = {
+  	val cant = Utility.length
+  	val prom1 = (Utility(cant-1) + Utility(cant-2))/2
+  	val prom2 = (Utility(0) + Utility(1))/2
+  	val dif = prom1 - prom2
+  	return dif
+  } 
   
 
   
